9d3f4b9de2d4ed7d8c8a17ffa4e8822c9336bb8c
Matheus Duarte <122234782+matheusduarte21@users.noreply.github.com>
2023-08-06 14:06:38 -0300

    atualização de variaveis e funções

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-function formatarNumero(numero, casasDecimais) {
+function FormatarNumero(numero, casasDecimais) {
     return numero.toLocaleString(undefined, {
       minimumFractionDigits: casasDecimais,
       maximumFractionDigits: casasDecimais,
@@ -7,95 +7,110 @@ function formatarNumero(numero, casasDecimais) {
     });
   }
 
-function Teste() {
-    var inputName = document.getElementById('input-name');
-    var valorInputUP = inputName.value.toUpperCase();
-    var inputValor = document.getElementById('input-valor');
-    var valorInputValor = inputValor.value;
-    var valor = parseFloat(valorInputValor);
-    var minhalista = document.getElementById('minhaLista');
-    var li = document.createElement('li');
-    minhalista.appendChild(li);
-    var Li_div = document.createElement('div');
+function CreatLi() {
+    const InputName = document.getElementById('input-name');
+    const ValorInputUP = InputName.value.toUpperCase();
+
+    const InputValor = document.getElementById('input-valor');
+    const ValorInputValor = InputValor.value;
+    const Valor = parseFloat(ValorInputValor);
+
+    const Minhalista = document.getElementById('minhaLista');
+    const Li = document.createElement('li');
+    Minhalista.appendChild(Li);
+
+    const Li_div = document.createElement('div');
     Li_div.classList.add('content-li');
-    li.appendChild(Li_div);
-    var div_content_li_title = document.createElement('div');
+    Li.appendChild(Li_div);
+
+    const div_content_li_title = document.createElement('div');
     div_content_li_title.classList.add('contente-li-title-name');
-    div_content_li_title.textContent = valorInputUP;
+    div_content_li_title.textContent = ValorInputUP;
     Li_div.appendChild(div_content_li_title);
-    var div_contente_li_title_valor = document.createElement('div');
+
+    const div_contente_li_title_valor = document.createElement('div');
     div_contente_li_title_valor.classList.add('contente-li-title-valor');
-    var span = document.createElement('span');
-    var format = formatarNumero(parseFloat(valorInputValor), 2)
-    span.textContent = format
-    if (valor > 0) {
-        span.classList.add('title-trans');
+    const Span = document.createElement('span');
+    const Format = FormatarNumero(parseFloat(ValorInputValor), 2)
+    Span.textContent = Format
+    if (Valor > 0) {
+        Span.classList.add('title-trans');
     }
     else {
-        span.classList.add('title-trans-red');
+        Span.classList.add('title-trans-red');
     }
-    div_contente_li_title_valor.appendChild(span);
+    div_contente_li_title_valor.appendChild(Span);
     Li_div.appendChild(div_contente_li_title_valor);
 }
 
-var Soma2 = 0;
-var Soma = 0;
+let Soma2 = 0;
+let Soma = 0;
 function Entradas() {
-    var inputValor = document.getElementById('input-valor');
-    var ValueInputValor = inputValor.value.trim();
-    var Rrendas = document.querySelector('.Rrendas');
+    const InputValor = document.getElementById('input-valor');
+    const ValueInputValor = InputValor.value.trim();
+    const Rrendas = document.querySelector('.Rrendas');
     if (ValueInputValor > '0') {
-        var valorNumerico = parseFloat(ValueInputValor);
+        let valorNumerico = parseFloat(ValueInputValor);
         if (valorNumerico > 0) {
             Soma += valorNumerico;
-            const numeroFormatado = formatarNumero(Soma, 2);
-           return Rrendas.textContent = numeroFormatado
+            const numeroFormatado = FormatarNumero(Soma, 2);
+            return Rrendas.textContent = numeroFormatado
         }
     }
 }
 
 
 function Saidas() {
-    var inputValor = document.getElementById('input-valor');
-    var ValueInputValor = inputValor.value.trim();
-    var Rdespesas = document.querySelector('.Rdepesas');
+    const InputValor = document.getElementById('input-valor');
+    const ValueInputValor = InputValor.value.trim();
+    const Rdespesas = document.querySelector('.Rdepesas');
     if (ValueInputValor < '0'){
-        var valorNumerico = parseFloat(ValueInputValor);
+        let valorNumerico = parseFloat(ValueInputValor);
         if (valorNumerico < 0) {
             Soma2 += valorNumerico;
-            const numeroFormatado = formatarNumero(Soma2, 2);
+            const numeroFormatado = FormatarNumero(Soma2, 2);
             return Rdespesas.textContent = numeroFormatado
         }
     }
 }
-function Total() {
-    var Total = document.querySelector('.color-title');
-    var ValorTotal = Soma + Soma2;
-    var format = formatarNumero(ValorTotal, 2)
-   return Total.textContent = format
-}
+
+
 function Transacao() {
     Entradas();
     Saidas();
     Total();
+
 }
-function Veri() {
-    var inputName = document.getElementById('input-name');
-    var ValueInputName = inputName.value;
-    var inputValor = document.getElementById('input-valor');
-    var ValueInputValor = inputValor.value;
-    if (ValueInputName === '') {
+
+function Total() {
+    const TotalElement = document.querySelector('.color-title');
+    const ValorTotal = Soma + Soma2;
+    const format = FormatarNumero(ValorTotal, 2)
+    return TotalElement.textContent = format
+
+}
+
+  function LimparFormulario(){
+    const InputNameValue = document.getElementById('input-name');
+    InputNameValue.value = '';
+    const InputValorValue = document.getElementById('input-valor');
+    InputValorValue.value = '';
+  }
+
+function Verificao() {
+    const InputName = document.getElementById('input-name');
+    const ValueInputName = InputName.value;
+    const InputValor = document.getElementById('input-valor');
+    const ValueInputValor = InputValor.value;
+    if (!ValueInputName) {
         alert('preencha todos os campos');
     }
-    else if (ValueInputValor === '') {
+    else if (!ValueInputValor) {
         alert("Preencha o campo");
     }
     else {
-        Teste();
+        CreatLi();
         Transacao();
-        var inputName_1 = document.getElementById('input-name');
-        inputName_1.value = '';
-        var inputValor_1 = document.getElementById('input-valor');
-        inputValor_1.value = '';
+        LimparFormulario()
     }
 }
--- script.ts
+++ script.ts
@@ -1,101 +1,116 @@
-function FormatarNumero(numero: number, casasDecimais: number) {
-  return numero.toLocaleString(undefined, {
-    minimumFractionDigits: casasDecimais,
-    maximumFractionDigits: casasDecimais,
-    style: 'currency',
-    currency: 'BRL'
-  });
+function FormatarNumero(numero, casasDecimais) {
+    return numero.toLocaleString(undefined, {
+      minimumFractionDigits: casasDecimais,
+      maximumFractionDigits: casasDecimais,
+      style: 'currency',
+      currency: 'BRL'
+    });
+  }
+
+function CreatLi() {
+    const InputName = document.getElementById('input-name');
+    const ValorInputUP = InputName.value.toUpperCase();
+
+    const InputValor = document.getElementById('input-valor');
+    const ValorInputValor = InputValor.value;
+    const Valor = parseFloat(ValorInputValor);
+
+    const Minhalista = document.getElementById('minhaLista');
+    const Li = document.createElement('li');
+    Minhalista.appendChild(Li);
+
+    const Li_div = document.createElement('div');
+    Li_div.classList.add('content-li');
+    Li.appendChild(Li_div);
+
+    const div_content_li_title = document.createElement('div');
+    div_content_li_title.classList.add('contente-li-title-name');
+    div_content_li_title.textContent = ValorInputUP;
+    Li_div.appendChild(div_content_li_title);
+
+    const div_contente_li_title_valor = document.createElement('div');
+    div_contente_li_title_valor.classList.add('contente-li-title-valor');
+    const Span = document.createElement('span');
+    const Format = FormatarNumero(parseFloat(ValorInputValor), 2)
+    Span.textContent = Format
+    if (Valor > 0) {
+        Span.classList.add('title-trans');
+    }
+    else {
+        Span.classList.add('title-trans-red');
+    }
+    div_contente_li_title_valor.appendChild(Span);
+    Li_div.appendChild(div_contente_li_title_valor);
 }
 
-function Teste() {
-  var inputName = document.getElementById('input-name');
-  var valorInputName = inputName.value;
-  var inputValor = document.getElementById('input-valor');
-  var valorInputValor = inputValor.value;
-  var valor = parseFloat(valorInputValor);
-  var Minhalista = document.getElementById('minhaLista');
-  var li = document.createElement('li');
-  Minhalista.appendChild(li);
-  var Li_div = document.createElement('div');
-  Li_div.classList.add('content-li');
-  li.appendChild(Li_div);
-  var div_content_li_title = document.createElement('div');
-  div_content_li_title.classList.add('contente-li-title-name');
-  div_content_li_title.textContent = valorInputName;
-  Li_div.appendChild(div_content_li_title);
-  var div_contente_li_title_valor = document.createElement('div');
-  div_contente_li_title_valor.classList.add('contente-li-title-valor');
-  var span = document.createElement('span');
-  var format = formatarNumero(parseFloat(valorInputValor), 2)
-  span.textContent = format
-  if (valor > 0) {
-      span.classList.add('title-trans');
-  }
-  else {
-      span.classList.add('title-trans-red');
-  }
-  div_contente_li_title_valor.appendChild(span);
-  Li_div.appendChild(div_contente_li_title_valor);
-}
-
-var Soma2 = 0;
-var Soma = 0;
+let Soma2 = 0;
+let Soma = 0;
 function Entradas() {
-  var inputValor = document.getElementById('input-valor');
-  var ValueInputValor = inputValor.value.trim();
-  var Rrendas = document.querySelector('.Rrendas');
-  if (ValueInputValor > '0') {
-      var valorNumerico = parseFloat(ValueInputValor);
-      if (valorNumerico > 0) {
-          Soma += valorNumerico;
-          const numeroFormatado = formatarNumero(Soma, 2);
-         return Rrendas.textContent = numeroFormatado
-      }
-  }
+    const InputValor = document.getElementById('input-valor');
+    const ValueInputValor = InputValor.value.trim();
+    const Rrendas = document.querySelector('.Rrendas');
+    if (ValueInputValor > '0') {
+        let valorNumerico = parseFloat(ValueInputValor);
+        if (valorNumerico > 0) {
+            Soma += valorNumerico;
+            const numeroFormatado = FormatarNumero(Soma, 2);
+            return Rrendas.textContent = numeroFormatado
+        }
+    }
 }
 
 
 function Saidas() {
-  var inputValor = document.getElementById('input-valor');
-  var ValueInputValor = inputValor.value.trim();
-  var Rdespesas = document.querySelector('.Rdepesas');
-  if (ValueInputValor < '0'){
-      var valorNumerico = parseFloat(ValueInputValor);
-      if (valorNumerico < 0) {
-          Soma2 += valorNumerico;
-          const numeroFormatado = formatarNumero(Soma2, 2);
-          return Rdespesas.textContent = numeroFormatado
-      }
-  }
+    const InputValor = document.getElementById('input-valor');
+    const ValueInputValor = InputValor.value.trim();
+    const Rdespesas = document.querySelector('.Rdepesas');
+    if (ValueInputValor < '0'){
+        let valorNumerico = parseFloat(ValueInputValor);
+        if (valorNumerico < 0) {
+            Soma2 += valorNumerico;
+            const numeroFormatado = FormatarNumero(Soma2, 2);
+            return Rdespesas.textContent = numeroFormatado
+        }
+    }
 }
-function Total() {
-  var Total = document.querySelector('.color-title');
-  var ValorTotal = Soma + Soma2;
-  var format = formatarNumero(ValorTotal, 2)
- return Total.textContent = format
-}
+
+
 function Transacao() {
-  Entradas();
-  Saidas();
-  Total();
+    Entradas();
+    Saidas();
+    Total();
+
 }
-function Veri() {
-  var inputName = document.getElementById('input-name');
-  var ValueInputName = inputName.value;
-  var inputValor = document.getElementById('input-valor');
-  var ValueInputValor = inputValor.value;
-  if (ValueInputName === '') {
-      alert('preencha todos os campos');
+
+function Total() {
+    const TotalElement = document.querySelector('.color-title');
+    const ValorTotal = Soma + Soma2;
+    const format = FormatarNumero(ValorTotal, 2)
+    return TotalElement.textContent = format
+
+}
+
+  function LimparFormulario(){
+    const InputNameValue = document.getElementById('input-name');
+    InputNameValue.value = '';
+    const InputValorValue = document.getElementById('input-valor');
+    InputValorValue.value = '';
   }
-  else if (ValueInputValor === '') {
-      alert("Preencha o campo");
-  }
-  else {
-      Teste();
-      Transacao();
-      var inputName_1 = document.getElementById('input-name');
-      inputName_1.value = '';
-      var inputValor_1 = document.getElementById('input-valor');
-      inputValor_1.value = '';
-  }
+
+function Verificao() {
+    const InputName = document.getElementById('input-name');
+    const ValueInputName = InputName.value;
+    const InputValor = document.getElementById('input-valor');
+    const ValueInputValor = InputValor.value;
+    if (!ValueInputName) {
+        alert('preencha todos os campos');
+    }
+    else if (!ValueInputValor) {
+        alert("Preencha o campo");
+    }
+    else {
+        CreatLi();
+        Transacao();
+        LimparFormulario()
+    }
 }
